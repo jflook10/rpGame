2ac2f5431bf8c504b9eb8f4cbc876ed822154aa5
Jillian Flook <jkenw10@gmail.com>
2017-03-19 22:16:03 -0500

    set framework files and basic html

---
+++ javascript/game.js
@@ -0,0 +1,12 @@
+// Each character in the game has 3 attributes: Health Points, Attack Power and Counter Attack Power.
+
+// Each time the player attacks, their character's Attack Power increases by its base Attack Power.
+
+// For example, if the base Attack Power is 6, each attack will increase the Attack Power by 6 (12, 18, 24, 30 and so on).
+// The enemy character only has Counter Attack Power.
+
+// Unlike the player's Attack Points, Counter Attack Power never changes.
+// The Health Points, Attack Power and Counter Attack Power of each character must differ.
+
+// No characters in the game can heal or recover Health Points.
+
