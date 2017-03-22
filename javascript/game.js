f45aa9affe93287dc40f57f9b393899f0b1ac4e2
Jillian Flook <jkenw10@gmail.com>
2017-03-21 21:30:49 -0500

    working rp game

--- javascript/game.js
+++ javascript/game.js
@@ -12,36 +12,40 @@ var player;
 var opponent;
 var winsVar = 0;
 var globalAP; 
-//var winsCount = $("#winsCount");
 
 
 //put the hp into the span of the closest char 
-
-	for (var i= 0; i < 4; i++){
-		var sectVal = $("section")[i]; //gets all sections at i
-		console.log(sectVal);
-		var spanVal = $("span")[i];  //set var for spans at i
-	  	//var healthPts = $(sectVal).attr("data-hp"); //gets the hp for the specific sect at i
-	  $(spanVal).html($(sectVal).attr("data-hp"));  //sets the inner html of the span for its related sect
-	}
+for (var i= 0; i < 4; i++){
+	var sectVal = $("section")[i]; //gets all sections at i
+	var spanVal = $("span")[i];  //set var for spans at i
+  	//var healthPts = $(sectVal).attr("data-hp"); //gets the hp for the specific sect at i
+  $(spanVal).html($(sectVal).attr("data-hp"));  //sets the inner html of the span for its related sect
+}
 
 
 //select player and highlight selected player with css.
 $(".char").click(function(){
 	if(selectPlayer){	
-		$(this).addClass("highlightPlay");
 		player = this;
+		
+		//highlight selected player and move to first space of characters
+		var playerLocation = player;
+		$(this).addClass("highlightPlay");
+		$(player).clone().prependTo(".firstRow");
+		$(playerLocation).hide();
+		
 		globalAP = +$(player).data("ap") //set global var CA to call additively later THIS ISNT WORKINNGGG!!!!!
 
 		//when player selected, move remaining players to right and prompt to select first attacker. 
-		$("#instructions").html("Please select your opponent");
+		$("#instructions").html("Select your first opponent");
 		console.log($(player).attr("id"));
 		selectOpponent = true;
 		selectPlayer =false;
+
 	}else if(!selectPlayer&&selectOpponent){
 		$(this).addClass("highlightOpp");
 		opponent = this;
-		$("#instructions").html("Attack your opponent")	
+		$("#instructions").html("Out-smart your opponent and reduce their HP to 0.")	
 		console.log($(opponent).attr("id"));
 		selectOpponent = false;	
 	}
@@ -61,9 +65,13 @@ $(".char").click(function(){
 		//get players AP
 		var playerAP= $(player).data("ap");
 		console.log(playerAP + "player AP");
+		
 		//decrease opponents HP with players AP
 		oppHP = oppHP - playerAP;
-		console.log()
+		//change opponent HP in the data and in html
+		$(opponent).data("hp", oppHP);
+		$(opponent).find("span").html(oppHP);
+		
 
 		//What to do when your opponents HP <= 0
 		if (oppHP <= 0){
@@ -82,20 +90,20 @@ $(".char").click(function(){
 
 
 			//when to select new opponent or stop game
-			if (winsVar <3) {
-				$("#instructions").html("You've subdued your opponent, select another")	
-				$(opponent)
+			if (winsVar === 1) {
+				$("#instructions").html("You've beat " + $(opponent).attr("id") + ". Select your next opponent");	
+				//$(opponent)
 				console.log(winsVar);
+			}
+			else if (winsVar === 2) {
+				$("#instructions").html("You've beat " + $(opponent).attr("id") + ". Select your final opponent")	
+				//$(opponent)
 			} else{
-				$("#instructions").html("Your mission has prevailed and you defeated all of your opponents");
+				$("#instructions").html("Yea!! You outsmarted your opponents and presented your bill. The honorable council members voted and it passed.");
 				return false;
 			}
 			
 		}
-
-		//change opponent HP in the data and in html
-		$(opponent).data("hp", oppHP);
-		$(opponent).find("span").html(oppHP);
 
 		//player hp - opponent counter attack
 		var playerHP = +$(player).data("hp");
